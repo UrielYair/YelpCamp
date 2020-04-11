634b2f6280c19689fcf0c3a86f54ab7995094900
Uriel Yair <uriel91@gmail.com>
2020-04-11 15:59:27 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+var campground = [
+    {name:"Salmon Creek", image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name:"Granite Hill", image:"https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name:"Mountain Goat's Rest", image:"https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80"},
+    {name:"Salmon Creek", image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name:"Granite Hill", image:"https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name:"Mountain Goat's Rest", image:"https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80"},
+    {name:"Salmon Creek", image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name:"Granite Hill", image:"https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name:"Mountain Goat's Rest", image:"https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80"}
+];
+
+app.get("/", function(req, res){
+    res.render("landing page");
+});
+
+app.get("/campgrounds", function(req, res){
+    res.render("campgrounds", {campground: campground});
+});
+
+app.post("/campgrounds", function(req, res){
+    var newCamp = {name: req.body.name, image: req.body.image};
+    campground.push(newCamp);
+    res.redirect("/campgrounds",)
+});
+
+app.get("/campgrounds/new",function(req, res){
+    res.render("newCamp");
+});
+
+app.listen(3000, function(){
+    console.log("The YelpCamp Server has Started.");
+});
+
